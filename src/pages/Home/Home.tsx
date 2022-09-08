8e8954c826d24e19e87711988a579d2b702d8a6b
Raul Marchan Catala <alountk@gmail.com>
2022-09-09 01:15:02 +0200

    table and styles working

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,10 +1,52 @@
 import React from 'react'
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid'
+import { People } from '@/data';
 
-export interface HomeInterface {}
+export interface HomeInterface {
+}
 
 const Home: React.FC<HomeInterface> = () => {
+    const pageSize = 5;
+    const columns = [
+        { 
+            field: 'name',
+            headerName: 'Name',
+            flex: 1,
+            minWidth: 150,
+            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+        },
+        { 
+            field: 'category',
+            headerName: 'Categories',
+            flex: 1,
+            minWidth: 150,
+            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+        },
+        { 
+            field: 'company',
+            headerName: 'Company',
+            flex: 1,
+            minWidth: 150,
+            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+        },
+        { 
+            field: 'levelOfHappiness',
+            headerName: 'Level Of Happiness',
+            flex: 1,
+            minWidth: 150,
+            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
+        },
+    ]
   return (
-    <div>Home</div>
+    <DataGrid 
+        disableColumnSelector 
+        disableSelectionOnClick 
+        autoHeight 
+        rowsPerPageOptions={[pageSize]} 
+        rows={People} 
+        getRowId={(row: any) => row.id}
+        {...{pageSize, columns}} 
+    />
   )
 }
 
