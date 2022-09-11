86c14ecfacc9142fdbd29720003681a7e7c9899d
Raul Marchan Catala <alountk@gmail.com>
2022-09-12 00:55:23 +0200

    add check favourites with add abstracts methods

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,53 +1,75 @@
-import React from 'react'
-import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid'
+import React, { useState } from 'react';
+import { DataGrid, GridRenderCellParams } from '@mui/x-data-grid';
 import { People } from '@/data';
+import { Person } from '@/models';
+import { Checkbox } from '@mui/material';
 
-export interface HomeInterface {
-}
+export interface HomeInterface {}
 
 const Home: React.FC<HomeInterface> = () => {
-    const pageSize = 5;
-    const columns = [
-        { 
-            field: 'name',
-            headerName: 'Name',
-            flex: 1,
-            minWidth: 150,
-            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-        },
-        { 
-            field: 'category',
-            headerName: 'Categories',
-            flex: 1,
-            minWidth: 150,
-            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-        },
-        { 
-            field: 'company',
-            headerName: 'Company',
-            flex: 1,
-            minWidth: 150,
-            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-        },
-        { 
-            field: 'levelOfHappiness',
-            headerName: 'Level Of Happiness',
-            flex: 1,
-            minWidth: 150,
-            renderCell: (params: GridRenderCellParams) => <>{params.value}</>
-        },
-    ]
-  return (
-    <DataGrid 
-        disableColumnSelector 
-        disableSelectionOnClick 
-        autoHeight 
-        rowsPerPageOptions={[pageSize]} 
-        rows={People} 
-        getRowId={(row: any) => row.id}
-        {...{pageSize, columns}} 
-    />
-  )
-}
+  const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
+  const pageSize = 5;
 
-export default Home
+  const findPerson = (person: Person) => !!selectedPeople.find((p) => p.id === person.id);
+  const filterPerson = (person: Person) => selectedPeople.filter((p) => p.id !== person.id);
+  const handleChange = (person: Person) => {
+    setSelectedPeople(findPerson(person) ? filterPerson(person) :  [...selectedPeople, person])
+
+  }
+  const columns = [
+    {
+      field: 'actions',
+      headerName: '',
+      minWidth: 50,
+      renderCell: (params: GridRenderCellParams) => 
+      <>
+        <Checkbox
+          size="small"
+          checked={findPerson(params.row)} 
+          onChange={() => handleChange(params.row)} />
+      </>
+      
+    },
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'category',
+      headerName: 'Categories',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'company',
+      headerName: 'Company',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'levelOfHappiness',
+      headerName: 'Level Of Happiness',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
+  return (
+    <DataGrid
+      disableColumnSelector
+      disableSelectionOnClick
+      autoHeight
+      rowsPerPageOptions={[pageSize]}
+      rows={People}
+      getRowId={(row: any) => row.id}
+      {...{ pageSize, columns }}
+    />
+  );
+};
+
+export default Home;
