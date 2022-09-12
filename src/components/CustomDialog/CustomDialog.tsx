0d29925c09af05a3e298b6f04b96a63e9e3b4bc8
Raul Marchan Catala <alountk@gmail.com>
2022-09-12 04:39:57 +0200

    add modal

---
+++ src/components/CustomDialog/CustomDialog.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+export interface CustomDialogInterface {}
+
+const CustomDialog: React.FC<CustomDialogInterface> = () => {
+	return <div >CustomDialog</div>;
+};
+
+export default CustomDialog;
+
