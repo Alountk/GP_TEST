682a85951fa012bb72c7cd3c5e021a5886d66b25
Raul Marchan Catala <alountk@gmail.com>
2022-09-12 04:40:13 +0200

    add modal

---
+++ src/components/Navbar/FavoriteTable/FavoriteTable.tsx
@@ -0,0 +1,73 @@
+import { Person } from '@/models';
+import { addFavorite } from '@/redux/states';
+import { AppStore } from '@/redux/store';
+import { Checkbox } from '@mui/material';
+import { GridRenderCellParams, DataGrid } from '@mui/x-data-grid';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+
+export interface FavoriteTableInterface {}
+
+const FavoriteTable: React.FC<FavoriteTableInterface> = () => {
+	const [selectedPeople, setSelectedPeople] = useState<Person[]>([]);
+  const pageSize = 5;
+  const dispatch = useDispatch();
+  const stateFavorites = useSelector((store: AppStore) => store.favorites);
+
+  const findPerson = (person: Person) =>
+    !!selectedPeople.find((p) => p.id === person.id);
+  const filterPerson = (person: Person) =>
+    selectedPeople.filter((p) => p.id !== person.id);
+  const handleChange = (person: Person) => {
+    const filteredPeople = findPerson(person)
+      ? filterPerson(person)
+      : [...selectedPeople, person];
+    dispatch(addFavorite(filteredPeople));
+    setSelectedPeople(filteredPeople);
+  };
+  const columns = [
+    {
+      field: 'name',
+      headerName: 'Name',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'category',
+      headerName: 'Categories',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'company',
+      headerName: 'Company',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+    {
+      field: 'levelOfHappiness',
+      headerName: 'Level Of Happiness',
+      flex: 1,
+      minWidth: 150,
+      renderCell: (params: GridRenderCellParams) => <>{params.value}</>,
+    },
+  ];
+  return (
+    <DataGrid
+      disableColumnSelector
+      disableSelectionOnClick
+      autoHeight
+      rowsPerPageOptions={[pageSize]}
+      rows={stateFavorites}
+      getRowId={(row: any) => row.id}
+      {...{ pageSize, columns }}
+    />
+  );
+};
+
+export default FavoriteTable;
+
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,21 +1,36 @@
-import { AppBar, Toolbar, Typography } from '@mui/material';
+import { AppBar, Button, IconButton, Toolbar, Typography } from '@mui/material';
 import Box from '@mui/material/Box';
 import React from 'react';
-
+import { CustomDialog } from '../CustomDialog';
+import { FavoriteTable } from './FavoriteTable';
+import { FavoriteBorderOutlined } from '@mui/icons-material';
+import { dialogOpenSubject$ } from '../CustomDialog/CustomDialog';
 
 export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => {
-	return  <Box sx={{ flexGrow: 1 }}>
-    <AppBar position="fixed" >
-      <Toolbar>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          Gentleman Programming React TEST
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  </Box>;
+  const handleClick = () => {
+    dialogOpenSubject$.setSubject = true;
+  };
+  return (
+    <>
+      <CustomDialog>
+        <FavoriteTable />
+      </CustomDialog>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar position="fixed">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              Gentleman Programming React TEST
+            </Typography>
+            <IconButton color="secondary" onClick={handleClick}>
+              <FavoriteBorderOutlined />{' '}
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </>
+  );
 };
 
 export default Navbar;
-
